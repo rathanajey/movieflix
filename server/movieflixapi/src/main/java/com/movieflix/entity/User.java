1694d5f9a5e46e8df11598934605de62b0ac29ee
rathanajey <rathanajey@gmail.com>
2016-09-10 11:34:48 -0700

    add user and title entities

---
+++ server/movieflixapi/src/main/java/com/movieflix/entity/User.java
@@ -0,0 +1,76 @@
+package com.movieflix.entity;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@NamedQueries({
+		@NamedQuery(name = "User.findAll", query = "SELECT e from User e ORDER BY e.email"),
+		@NamedQuery(name = "User.findByEmail", query = "SELECT e from User e where e.email=:pEmail")
+})
+public class User {
+
+	@Id
+	private String id;
+
+	private String firstName;
+	private String lastName;
+	private String password;
+
+
+
+	@Column(unique = true)
+	private String email;
+
+	public User() {
+		id = UUID.randomUUID().toString();
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+}
